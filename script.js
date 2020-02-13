d48f0fca618f57798b8a7737bcd72ca36b8b3f6f
jrock312 <guevarra_joel@yahoo.com>
2020-02-12 20:06:52 -0800

    add JS local storage and classes

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    $(".saveBtn").on("click", function(){
+        var value =$(this).siblings(".description").val();
+        var time =$(this).parent().attr("id");
+
+        localStorage.setItem(time,value);
+    })
+
+    function hourUpdater(){
+        var currentHour = moment().hours();
+        $(".time-block").each(function(){
+            var blockHour = parseInt($(this).attr("id").split("-")[1])
+            if (blockHour < currentHour){
+                $(this).addClass("past")
+
+            } 
+            else if (blockHour===currentHour){
+
+                $(this).removeClass("past");
+                $(this).addClass("present");
+
+            }
+            else{ $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+
+            }
+            
+        })
+
+    }
+    hourUpdater()
+var interval =setInterval(hourUpdater,15000)
+
+for (var i = 9; i < 18; i++){
+
+    $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i));
+}
+
+})
